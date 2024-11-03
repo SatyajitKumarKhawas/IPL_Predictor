70c4288167913852f82f18b02a9c54f08d942757
SATYAJIT KUMAR KHAWAS <144238449+SatyajitKumarKhawas@users.noreply.github.com>
2024-11-03 17:17:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Set up custom CSS for the background image
+page_bg_img = '''
+<style>
+.stApp {
+    background-image: url("https://images.unsplash.com/photo-1587385789097-0197a7fbd179?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y3JpY2tldCUyMGJhbGx8ZW58MHx8MHx8fDA%3D");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+</style>
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# Continue with the rest of the code
+teams = ['Sunrisers Hyderabad',
+ 'Mumbai Indians',
+ 'Royal Challengers Bangalore',
+ 'Kolkata Knight Riders',
+ 'Kings XI Punjab',
+ 'Chennai Super Kings',
+ 'Rajasthan Royals',
+ 'Delhi Capitals']
+
+cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
+       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+       'Sharjah', 'Mohali', 'Bengaluru']
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+st.title('IPL Win Predictor')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    batting_team = st.selectbox('Select the batting team', sorted(teams))
+with col2:
+    bowling_team = st.selectbox('Select the bowling team', sorted(teams))
+
+selected_city = st.selectbox('Select host city', sorted(cities))
+
+target = st.number_input('Target')
+
+col3, col4, col5 = st.columns(3)
+
+with col3:
+    score = st.number_input('Score')
+with col4:
+    overs = st.number_input('Overs completed')
+with col5:
+    wickets = st.number_input('Wickets out')
+
+if st.button('Predict Probability'):
+    runs_left = target - score
+    balls_left = 120 - (overs * 6)
+    wickets = 10 - wickets
+    crr = score / overs
+    rrr = (runs_left * 6) / balls_left
+
+    input_df = pd.DataFrame({
+        'batting_team': [batting_team],
+        'bowling_team': [bowling_team],
+        'city': [selected_city],
+        'runs_left': [runs_left],
+        'balls_left': [balls_left],
+        'wickets': [wickets],
+        'total_runs_x': [target],
+        'crr': [crr],
+        'rrr': [rrr]
+    })
+
+    result = pipe.predict_proba(input_df)
+    loss = result[0][0]
+    win = result[0][1]
+    st.header(batting_team + "- " + str(round(win * 100)) + "%")
+    st.header(bowling_team + "- " + str(round(loss * 100)) + "%")
